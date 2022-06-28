b02de60bb3b2a648277ac004210e8a06ff62b823
Arjun Gupta <arjuntheprogrammer@gmail.com>
2022-06-28 22:31:23 +0530

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import { Nav } from "./Nav";
+import headerStyles from "../styles/Header.module.css";
+
+export const Header = () => {
+  return (
+    <div>
+      <h1 className={headerStyles.title}>
+        <span>WebDev</span> News
+      </h1>
+      <p className={headerStyles.description}>
+        Keep up to date with the latest web dev news.
+      </p>
+    </div>
+  );
+};
